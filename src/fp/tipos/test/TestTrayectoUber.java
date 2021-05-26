f2466d289856c877f3be564880eafdec3bf97dee
Miguel Figueroa Escribano <migfigesc@alum.us.es>
2021-05-26 22:19:31 +0200

    Update TestTrayectoUber

--- src/fp/tipos/test/TestTrayectoUber.java
+++ src/fp/tipos/test/TestTrayectoUber.java
@@ -1,17 +1,9 @@
 package fp.tipos.test;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import fp.tipos.Datos;
-import fp.tipos.FactoriaTrayectos;
 import fp.tipos.ModeloCoche;
 import fp.tipos.TrayectoUber;
-import fp.utiles.Ficheros;
 
-import static fp.tipos.ModeloCoche.FIAT_TIPO;
+import java.time.LocalDate;
 
 public class TestTrayectoUber {
 	
@@ -24,7 +16,8 @@ public class TestTrayectoUber {
 			System.out.println("\nTEST DEL CONSTRUCTOR");
 			try {
 
-				TrayectoUber trayecto1 = new TrayectoUber("Puerta Jerez", "Apeadero San Bernardo", LocalDate.of(2021, 04, 23), 340,
+				TrayectoUber trayecto1 = new TrayectoUber("Puerta Jerez", "Apeadero San Bernardo",
+						LocalDate.of(2021, 04, 23), 340,
 				220, 600, ModeloCoche.PEUGEOT_308, true);
 				System.out.println("TrayectoUber:" + trayecto1);
 
