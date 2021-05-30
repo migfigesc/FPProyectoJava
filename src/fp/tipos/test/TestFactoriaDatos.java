536c92975ed90e75b087069c52443f5f64948ee1
Miguel Figueroa Escribano <migfigesc@alum.us.es>
2021-05-30 18:30:35 +0200

    Update and fix TestFactoriaDatos

--- src/fp/tipos/test/TestFactoriaDatos.java
+++ src/fp/tipos/test/TestFactoriaDatos.java
@@ -2,6 +2,7 @@ package fp.tipos.test;
 
 import fp.tipos.FactoriaTrayectos;
 import fp.tipos.Datos;
+import fp.tipos.ModeloCoche;
 
 public class TestFactoriaDatos {
 
@@ -11,6 +12,7 @@ public class TestFactoriaDatos {
     public static void main(String[] args) {
         testCreacionDatos();
         testExisteTrayectoDestino();
+        testGetNumeroTrayectoModeloCoche();
 
     }
 
@@ -25,10 +27,23 @@ public class TestFactoriaDatos {
 
     private static void testExisteTrayectoDestino() {
         System.out.println("\nTest de existeTrayectoDestino");
+        String trayectoDestino = "0 Rue Saint-Sauveur, Paris";
         try {
-            System.out.println(DATOS.existeTrayectoDestino("0 Rue Saint-Sauveur"));
+            System.out.println("Existe el trayecto con destino: " + trayectoDestino + ": " +
+                    DATOS.existeTrayectoDestino(trayectoDestino));
         } catch (Exception e) {
             System.out.println("Excepción capturada:\n" + e);
+        }
+    }
+
+    private static void testGetNumeroTrayectoModeloCoche() {
+        System.out.println("\nTest de getNumeroTrayectoModeloCoche:");
+        ModeloCoche modeloCocheTest = ModeloCoche.PEUGEOT_308;
+        try {
+            System.out.println("Existen " + DATOS.getNumeroTrayectoModeloCoche(modeloCocheTest)+ " trayectos" +
+                    " donde el coche más usado es el " + modeloCocheTest);
+        } catch (Exception e) {
+            System.out.println("Excepción capturada \n" + e);
         }
     }
 }
