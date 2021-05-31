124adcd31fe2fa8bbc8de169ab0e122583ca040b
Miguel Figueroa Escribano <migfigesc@alum.us.es>
2021-05-31 20:14:03 +0200

    Tratamientos secuenciales I, II y Enunciado.docx

--- src/fp/tipos/Datos.java
+++ src/fp/tipos/Datos.java
@@ -1,7 +1,7 @@
 package fp.tipos;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Datos {
 	private List<Trayecto> trayectos;
@@ -45,6 +45,33 @@ public class Datos {
 	public Long getNumeroTrayectoModeloCoche(ModeloCoche modelo) {
 		return trayectos.stream()
 				.filter(x -> x.getCocheMasUsado().equals(modelo)).count();
+	}
+
+	public Set<ModeloCoche> getModeloCochesTrayectos() {
+		return trayectos.stream()
+				.map(Trayecto::getCocheMasUsado)
+				.collect(Collectors.toSet());
+	}
+
+	//map y sus variantes mapToInt, mapToLong y mapToDouble. (Function) + distinct, limit
+
+	public Trayecto getTrayectoMayorDuracionMedia() {
+		Comparator<Trayecto> cmp = Comparator.comparing(Trayecto::getMediaDuracionTrayecto);
+		return trayectos.stream()
+				.max(cmp)
+				.get();
+	}
+
+	public void cambiaPagoEfectivoDestino(String destino, Boolean pagoEfectivo, Boolean nuevoPagoEfectivo) {
+		trayectos.stream()
+				.filter(x -> x.getPagoEfectivo().equals(pagoEfectivo)&&
+						x.getDestinoTrayecto().equals(destino))
+				.forEach(x->x.setPagoEfectivo(nuevoPagoEfectivo));
+	}
+
+	public Map<ModeloCoche, List<Trayecto>> getTrayectosPorModeloCoche() {
+		return trayectos.stream()
+				.collect(Collectors.groupingBy(Trayecto::getCocheMasUsado));
 	}
 
 }
--- src/fp/tipos/test/TestFactoriaDatos.java
+++ src/fp/tipos/test/TestFactoriaDatos.java
@@ -10,9 +10,12 @@ public class TestFactoriaDatos {
             "correctamente", "data/dataset_EDITADO.csv");
 
     public static void main(String[] args) {
-        testCreacionDatos();
-        testExisteTrayectoDestino();
-        testGetNumeroTrayectoModeloCoche();
+        //testCreacionDatos();
+        //testExisteTrayectoDestino();
+        //testGetNumeroTrayectoModeloCoche();
+        //testGetModeloCochesTrayectos();
+        //testGetTrayectoMayorDuracionMedia();
+        testGetTrayectosPorModeloCoche();
 
     }
 
@@ -45,6 +48,53 @@ public class TestFactoriaDatos {
         } catch (Exception e) {
             System.out.println("Excepción capturada \n" + e);
         }
+    }
+
+    private static void testGetModeloCochesTrayectos() {
+        System.out.println("\nTest de getModeloCochesTrayectos");
+        try {
+            System.out.println("Los diferentes modelos disponibles de coches en la flota de " +
+                    "Uber son: " + DATOS.getModeloCochesTrayectos());
+        } catch (Exception e) {
+            System.out.println("Excepción capturada \n" + e);
+        }
+    }
+
+    //test map y sus variantes mapToInt, mapToLong y mapToDouble. (Function) + distinct, limit
+
+    private static void testGetTrayectoMayorDuracionMedia() {
+        System.out.println("\nTest de getTrayectoMayorDuracionMedia");
+        try {
+            System.out.println("El trayecto con mayor duración media es: " + DATOS.getTrayectoMayorDuracionMedia());
+        } catch (Exception e) {
+            System.out.println("Excepción capturada \n" + e);
+        }
+    }
+
+    private static void testCambiaOrigenTrayectoSegunModeloCoche() {
+        ModeloCoche modelo = ModeloCoche.MAZDA_3;
+        String origen = "Arc de triomphe du Carrousel, Paris";
+        String nuevoOrigen = " Plaza de Armas, Sevilla";
+
+        System.out.println("Test de cambiaOrigenTrayectoSegunModeloCoche");
+        try {
+            System.out.println("Número de trayectos de " + modelo + " desde " + origen +" : " +
+                    DATOS.getNumeroTrayectoModeloCoche(modelo));
+
+        } catch (Exception e){
+            System.out.println("Excepción capturada:\n" + e);
+        }
+
+    }
+
+    private static void testGetTrayectosPorModeloCoche() {
+        System.out.println("\nTest de getTrayectosPorModeloCoche");
+        try {
+            System.out.print(DATOS.getTrayectosPorModeloCoche());
+        } catch (Exception e) {
+            System.out.println("Excepción capturada\n" + e);
+        }
+
     }
 }
 
